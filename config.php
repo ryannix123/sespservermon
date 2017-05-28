47821769b27e23a2bb8d7dc2cba8795ec96f10a2
Ryan T Nix <ryan.nix@gmail.com>
2017-05-28 09:32:43 -0500

    Config upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_DB_USER', 'userL3Y');
+define('PSM_DB_PASS', 'KEInopyQ2LDyp2yG');
+define('PSM_DB_NAME', 'phpservermon');
+define('PSM_DB_HOST', '10.131.0.158');
+define('PSM_DB_PORT', '3306');
+define('PSM_BASE_URL', 'http://http://php-servermon.os.apps.northwestern.edu');
