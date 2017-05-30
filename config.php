6e0b666476be10536b12c1fd24fff9e73489166a
Ryan T Nix <ryan.nix@gmail.com>
2017-05-30 07:24:24 -0500

    New config file

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define('PSM_DB_PREFIX', 'psm_');
 define('PSM_DB_USER', 'userL3Y');
 define('PSM_DB_PASS', 'KEInopyQ2LDyp2yG');
 define('PSM_DB_NAME', 'phpservermon');
-define('PSM_DB_HOST', '10.131.0.158');
+define('PSM_DB_HOST', '10.131.0.167');
 define('PSM_DB_PORT', '3306');
 define('PSM_BASE_URL', 'http://php-servermon.os.apps.northwestern.edu');
